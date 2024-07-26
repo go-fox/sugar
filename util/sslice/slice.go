c5b19210270e2b16cfc3067fbc724eb573337ed7
liuqin <15023989265@163.com>
2024-07-26 15:22:31 +0800

    feat:增加slice的ContainBy,ContainSubSlice,Chunk方法

--- util/sslice/slice.go
+++ util/sslice/slice.go
@@ -38,3 +38,72 @@ func Contain[T comparable](slice []T, target T) bool {
 
 	return false
 }
+
+// ContainBy returns true if predicate function return true.
+//
+//	@param slice []T
+//	@param predicate func(item T) bool
+//	@return bool
+//	@player
+func ContainBy[T any](slice []T, predicate func(item T) bool) bool {
+	for _, item := range slice {
+		if predicate(item) {
+			return true
+		}
+	}
+
+	return false
+}
+
+// ContainSubSlice check if the slice contains a given subslice or not.
+//
+//	@param slice []T
+//	@param subSlice []T
+//	@return bool
+//	@player
+func ContainSubSlice[T comparable](slice, subSlice []T) bool {
+	if len(subSlice) == 0 {
+		return true
+	}
+	if len(slice) == 0 {
+		return false
+	}
+
+	elementMap := make(map[T]struct{}, len(slice))
+	for _, item := range slice {
+		elementMap[item] = struct{}{}
+	}
+
+	for _, item := range subSlice {
+		if _, ok := elementMap[item]; !ok {
+			return false
+		}
+	}
+	return true
+}
+
+// Chunk creates a slice of elements split into groups the length of size.
+//
+//	@param slice []T
+//	@param size int
+//	@return [][]T
+//	@player
+func Chunk[T any](slice []T, size int) [][]T {
+	result := [][]T{}
+
+	if len(slice) == 0 || size <= 0 {
+		return result
+	}
+
+	for _, item := range slice {
+		l := len(result)
+		if l == 0 || len(result[l-1]) == size {
+			result = append(result, []T{})
+			l++
+		}
+
+		result[l-1] = append(result[l-1], item)
+	}
+
+	return result
+}
